fa30abdd6a660fdab93ca31dbd85a2a450f28e0b
Sowmya Logaswamy <sowmyalogaswamy@gmail.com>
2017-06-30 17:49:10 -0700

    Do changes in the user interface to make it display in an order

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,7 @@ public class App {
 
   public static void main(String[] args)
   {
-    Console myConsole = System.console();
+    Console myconsole = System.console();
 
     EventPlanning birthday = new EventPlanning(25,"grand", "soft drinks", "Rock", "Magic Shoes");
     EventPlanning wedding = new EventPlanning(100,"grand", "soft drinks", "Pop", "Wand");
@@ -22,26 +22,21 @@ public class App {
 
          System.out.println("-----------------EVENT PLANNERS-----------");
          System.out.println("Welcome!! You can choose any one of the options: All Events , Create Events, Exit");
-        //  System.out.println("1. If you would like to look at existing plans enter: plan");
-        //  System.out.println("2. If you would like to create your own plan enter: create");
-        //  System.out.println("3. If you would like to enter a coupon/discount code enter: discount");
-        //  System.out.println("4. If you would like to see current promotions enter: promotions");
-        //  System.out.println("5. If you would like to leave enter: exit.");
          String userInput = myconsole.readLine();
 
-
          if (userInput.equals("All Events")) {
-           for (EventPlanning plan : allPlans ) {
-             System.out.println("----------Available Plan--------------");
-             System.out.println("Event Size: " + plan.getEventSize());
-             System.out.println("Foods Available: " + plan.getFoodTypes());
-             System.out.println("Beverages Available: " + plan.getBeverageTypes());
-             System.out.println("Music Bands Available: " + plan.getMusicBandTypes());
-             System.out.println("Decorations Type: " + plan.getAccessoriesAvailable());
-             System.out.println("Total Cost: " + plan.totalCost());
+           System.out.println("----------Available Event--------------");
+           for (EventPlanning eventplan : allPlans ) {
+             System.out.println("Event Size: " + String.valueOf(eventplan.getEventSize()));
+             System.out.println("Foods Available: " + String.valueOf(eventplan.getFoodTypes()));
+             System.out.println("Beverages Available: " + String.valueOf(eventplan.getBeverageTypes()));
+             System.out.println("Music Bands Available: " + String.valueOf(eventplan.getMusicBandTypes()));
+             System.out.println("Decorations Type: " + String.valueOf(eventplan.getAccessoriesAvailable()));
+             System.out.println("Total Cost: " + String.valueOf(eventplan.totalCost()));
              System.out.println(" ");
            }
          } else if(userInput.equals("Create Events")) {
+
            System.out.println("How many will attend the event?");
            int userEventSize = Integer.parseInt(myconsole.readLine());
            System.out.println("Is it grand or simple event?");
@@ -57,16 +52,21 @@ public class App {
            System.out.println(" ");
 
            System.out.println("-------Here is your Event Plan for today!!!---------");
-           System.out.println("Event Size: " + plan.getEventSize());
-           System.out.println("Foods Available: " + plan.getFoodTypes());
-           System.out.println("Beverages Available: " + plan.getBeverageTypes());
-           System.out.println("Music Bands Available: " + plan.getMusicBandTypes());
-           System.out.println("Decorations Type: " + plan.getAccessoriesAvailable());
-           System.out.println("Total Cost: " + plan.totalCost());
-
-         } else if(userInput.equals("exit")){
+           System.out.println("Event Size: " + String.valueOf(eventplan.getEventSize()));
+           System.out.println("Foods Available: " + String.valueOf(eventplan.getFoodTypes()));
+           System.out.println("Beverages Available: " + String.valueOf(eventplan.getBeverageTypes()));
+           System.out.println("Music Bands Available: " + String.valueOf(eventplan.getMusicBandTypes()));
+           System.out.println("Decorations Type: " + String.valueOf(eventplan.getAccessoriesAvailable()));
+           System.out.println("Total Cost: " + String.valueOf(eventplan.totalCost()));
+         }
+         else if(userInput.equals("exit"))
+         {
            programRun = false;
          }
-       } //while loop end
+         } //while loop end
+         else {
+           System.out.println("Sorry,we dont recognize");
+         }
+
      }
-}
+   }
